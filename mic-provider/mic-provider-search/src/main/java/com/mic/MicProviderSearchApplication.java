042d4aad174c6691e41aa1a20f77fe8fed86b57a
yao <pengfei.yao@yunlsp.com>
2021-01-25 14:19:48 +0800

    集成elasticsearch

---
+++ mic-provider/mic-provider-search/src/main/java/com/mic/MicProviderSearchApplication.java
@@ -0,0 +1,13 @@
+package com.mic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicProviderSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicProviderSearchApplication.class, args);
+    }
+
+}
